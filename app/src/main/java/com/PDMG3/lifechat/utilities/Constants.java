7e7a3f90b2d98249ed7c362ec125c6a851889746
CL21005 <cl21005@ues.edu.sv>
2025-05-19 00:08:16 -0600

    INICIAL COMMIT

---
+++ app/src/main/java/com/PDMG3/lifechat/utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.PDMG3.lifechat.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME = "nombre";
+    public static final String KEY_EMAIL = "email";
+    public  static  final  String KEY_PASSWORD = "password";
+    public static final String KEY_REFERENCE_NAME = "chatAppPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+}
