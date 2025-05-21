1dc695e2ad167d91c909a081401f07cd1829dabc
EnriqueCanales <carlos.canales@deitgroup.com>
2025-05-21 16:56:04 -0600

    Avance de vista chats

---
+++ app/src/main/java/com/PDMG3/lifechat/activities/UsersActivity.java
@@ -0,0 +1,18 @@
+package com.PDMG3.lifechat.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.PDMG3.lifechat.R;
+
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+    }
+}
